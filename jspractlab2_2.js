a69da823078f900507f8fbe1c6b6bf29f59b6e3b
gandharvdamani <gandharvdamani@gmail.com>
2017-03-07 15:36:49 +0530

    Create jspractlab2_2.js

---
+++ jspractlab2_2.js
@@ -0,0 +1,39 @@
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += " & Lovin' it!";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+  else{
+    var title1 = 
+      document
+        .querySelector("#title1")
+        .textContent;
+    title1 += "";
+    document
+        .querySelector("h1")
+        .textContent = title1;
+  }
+
+}
