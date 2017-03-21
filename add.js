43311c104e5c48c7e4f8526578f9b7750b6da062
gandharvdamani <gandharvdamani@gmail.com>
2017-03-21 11:26:35 +0530

    Update add.js

--- add.js
+++ add.js
@@ -17,5 +17,19 @@ document.addEventListener("DOMContentLoaded",
                 .innerHTML = "<h2>" + message + "</h2>";
             });
       });
+  document.querySelector("button1")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("num1.json", 
+            function (res) {
+              var message = "Subtraction is : ";
+               var message1= res.num1 - res.num2;
+              message += message1;
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
   }
 );
