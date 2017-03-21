4d0fb5ba58b8b56edd7916c7cf744594d9200a32
gandharvdamani <gandharvdamani@gmail.com>
2017-03-21 15:32:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
-    
-    // Unobtrusive event binding
-    document.getElementById("add")
+  var name =
+       document.getElementById("name").value;
+      document.querySelector("button")
       .addEventListener("click", function () {
         
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("num1.json", 
             function (res) {
-              var message = "Marks are: ";
-               var message1= res.Maths + res.WP;
-              message += message1;
+          if (name===res.student1){
+               var message = "Marks are: ";
+               var message1= res.Maths;
+               var message2= res.WP;
+              message= message+ message1+message2;
+          }
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
-            });
+        });
+        
       });
+          
